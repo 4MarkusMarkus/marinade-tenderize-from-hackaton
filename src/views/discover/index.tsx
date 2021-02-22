d8c4105119d41be102ac0431ce28b9fd6e118aae
Michael Repetny <mrepetny@gmail.com>
2021-02-22 18:15:31 +0100

    feat(homepage): update copy, wallet signin

--- src/views/discover/index.tsx
+++ src/views/discover/index.tsx
@@ -1,102 +1,106 @@
-import React, { useCallback } from "react";
-import { Button, Card} from "antd";
-import { Link } from "react-router-dom";
-import { useConnection } from "../../contexts/connection";
-import { useWallet } from "../../contexts/wallet";
-import { LAMPORTS_PER_SOL } from "@solana/web3.js";
-import { notify } from "../../utils/notifications";
-import { ConnectButton } from "./../../components/ConnectButton";
-import { LABELS } from "../../constants";
-import {Line} from 'react-chartjs-2';
+import React from 'react';
+import { Button, Card } from 'antd';
+import { Link } from 'react-router-dom';
+import { Line } from 'react-chartjs-2';
 
-const solanaLogo = require ("../../img/solanaLogo.svg")
+const solanaLogo = require('../../img/solanaLogo.svg');
 
 export const DiscoverView = () => {
-  const connection = useConnection();
-  const { publicKey } = useWallet();
+  // const connection = useConnection();
+  // const { publicKey } = useWallet();
 
   const state = {
-    labels: ['January', 'February', 'March',
-             'April', 'May'],
+    labels: ['January', 'February', 'March', 'April', 'May'],
     datasets: [
       {
         label: 'Price',
         backgroundColor: 'rgba(75,192,192,1)',
         borderColor: 'rgba(0,0,0,1)',
         borderWidth: 2,
-        data: [65, 59, 80, 81, 56]
-      }
-    ]
-  }
-
-
-  const airdrop = useCallback(() => {
-    if (!publicKey) {
-      return;
-    }
-
-    connection.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL).then(() => {
-      notify({
-        message: LABELS.ACCOUNT_FUNDED,
-        type: "success",
-      });
-    });
-  }, [publicKey, connection]);
+        data: [65, 59, 80, 81, 56],
+      },
+    ],
+  };
 
   return (
-    <div style={{display:"flex", width:"100%", justifyContent: "center"}}>
-      <div style={{width:"60%", marginRight:"20px", textAlign:"left", display:"flex", flexDirection:"column", justifyContent: "space-between"}}>
-          <div>
-            <h1>tSOL Share Price</h1>
-            <Card className="card">
-              <Line
-                data={state}
-                options={{
-                  legend: {
-                    display: false
-                  }
-                }}
-              />
-            </Card>
-          </div>
-          <div>
-            <h1>tSOL Vault Rewards</h1>
-            <Card className="card">
-              <Line
-            data={state}
-              />
-            </Card>
-          </div>
-          <div>
-            <h1>About SOL</h1>
-            <Card className="card">
-              <Line
+    <div style={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
+      <div
+        style={{
+          width: '60%',
+          marginRight: '20px',
+          textAlign: 'left',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div>
+          <h1>tSOL Share Price</h1>
+          <Card className='card'>
+            <Line
               data={state}
-              />
-            </Card>
-          </div>
+              options={{
+                legend: {
+                  display: false,
+                },
+              }}
+            />
+          </Card>
+        </div>
+        <div>
+          <h1>tSOL Vault Rewards</h1>
+          <Card className='card'>
+            <Line data={state} />
+          </Card>
+        </div>
+        <div>
+          <h1>About SOL</h1>
+          <Card className='card'>
+            <Line data={state} />
+          </Card>
+        </div>
       </div>
-      
-      <div className = "card" style={{height: "450px", width:"30%", position: "sticky", top:0}}>
-      <Card className="card" style={{ width: "100%", height: "100%"}}>
-            <div style={{height: "100%",display: "flex", flexDirection:"column", justifyContent:"space-between"}}>
-              <div>
-                <Button>Deposit</Button>
-                <Button>Withdraw</Button>
-              </div>
-              <div>
-                <img src={solanaLogo} alt="solana logo" style={{maxWidth:"30%"}}/>
-                <h2 className="subtext" style={{marginTop:"5px"}}>SOLANA</h2>
-              </div>
-              <div style={{marginTop: "5px", marginBottom:"5px"}}>
-                <h2 className="title" style={{marginBottom: 0}}>15.5%</h2>
-                <h3 style={{marginBottom: 0}}>Vault Rewards</h3>
-                <h5>(projected APY)</h5>
-              </div>
-              <Link to="/discover">
-                <Button className="tenderButton tenderButtonShade">Discover</Button>
-              </Link>
+
+      <div
+        className='card'
+        style={{ height: '450px', width: '30%', position: 'sticky', top: 0 }}
+      >
+        <Card className='card' style={{ width: '100%', height: '100%' }}>
+          <div
+            style={{
+              height: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+            }}
+          >
+            <div>
+              <Button>Deposit</Button>
+              <Button>Withdraw</Button>
             </div>
+            <div>
+              <img
+                src={solanaLogo}
+                alt='solana logo'
+                style={{ maxWidth: '30%' }}
+              />
+              <h2 className='subtext' style={{ marginTop: '5px' }}>
+                SOLANA
+              </h2>
+            </div>
+            <div style={{ marginTop: '5px', marginBottom: '5px' }}>
+              <h2 className='title' style={{ marginBottom: 0 }}>
+                15.5%
+              </h2>
+              <h3 style={{ marginBottom: 0 }}>Vault Rewards</h3>
+              <h5>(projected APY)</h5>
+            </div>
+            <Link to='/discover'>
+              <Button className='tenderButton tenderButtonShade'>
+                Discover
+              </Button>
+            </Link>
+          </div>
         </Card>
       </div>
     </div>
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,17 +1,17 @@
-import { LAMPORTS_PER_SOL } from "@solana/web3.js";
-import { Button, Col, Row, Card } from "antd";
-import React, { useEffect, useMemo } from "react";
-import { Link } from "react-router-dom";
-import { ConnectButton } from "../../components/ConnectButton";
-import { useNativeAccount } from "../../contexts/accounts";
-import { useConnectionConfig } from "../../contexts/connection";
-import { useMarkets } from "../../contexts/market";
-import { formatNumber } from "../../utils/utils";
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { Col, Row, Card } from 'antd';
+import React, { useEffect, useMemo } from 'react';
+import { useHistory } from 'react-router-dom';
+import { ConnectButton } from '../../components/ConnectButton';
+import { useNativeAccount } from '../../contexts/accounts';
+import { useConnectionConfig } from '../../contexts/connection';
+import { useMarkets } from '../../contexts/market';
+import { formatNumber } from '../../utils/utils';
 
-const stakeHammer = require("../../img/stakeHammer.svg");
-const potionHero = require("../../img/potionHero.svg");
-const friends = require("../../img/friends.svg");
-const solanaLogo = require("../../img/solanaLogo.svg");
+const stakeHammer = require('../../img/stakeHammer.svg');
+const potionHero = require('../../img/potionHero.svg');
+const friends = require('../../img/friends.svg');
+const solanaLogo = require('../../img/solanaLogo.svg');
 
 export const HomeView = () => {
   const { marketEmitter, midPriceInUSD } = useMarkets();
@@ -22,6 +22,11 @@ export const HomeView = () => {
     () => formatNumber.format((account?.lamports || 0) / LAMPORTS_PER_SOL),
     [account]
   );
+
+  let history = useHistory();
+  const handleClick = (to: string) => {
+    history.push(to);
+  };
 
   useEffect(() => {
     const refreshTotal = () => {};
@@ -38,84 +43,141 @@ export const HomeView = () => {
   }, [marketEmitter, midPriceInUSD, tokenMap]);
 
   return (
-    <Row gutter={[48, 48]} align="middle">
-      <Col span={24} style={{height: "90vh", display: "flex", alignItems:"center", justifyContent:"center"}}>
+    <Row gutter={[48, 48]} align='middle'>
+      <Col
+        span={24}
+        style={{
+          height: '90vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
         <div>
-          <h2 className="title" style={{ marginTop: "1em", marginBottom:0 }}>
-                  Don&apos;t just stake me,
+          <h2 className='title' style={{ marginTop: '1em', marginBottom: 0 }}>
+            The easiest way to stake your tokens
           </h2>
-          <h2 className="title" style={{ marginTop: 0}} >
-            <span style={{ color: "#4E66DE" }}>Tenderize</span> me first
+          <h2 className='title' style={{ marginTop: 0 }}>
+            and get rewards
           </h2>
-          <h3 className="subtext" style={{marginBottom: "2em"}}>
-          Making
-                  <span style={{ color: "#4E66DE", fontWeight: 900 }}>
-                    {" stake easier "}
-                  </span>
-                  to chew.
-                  </h3>
-          <img width="150" src={stakeHammer} alt="logo" />
+          <h3 className='subtext' style={{ marginBottom: '2em' }}>
+            Enjoy automatic reinvestment of rewards and immediate access to
+            staked tokens.
+            {/* Making
+            <span style={{ color: '#4E66DE', fontWeight: 900 }}>
+              {' stake easier '}
+            </span>
+            to chew. */}
+          </h3>
+          <img width='150' src={stakeHammer} alt='logo' />
         </div>
       </Col>
-      <Col span={22} offset={1} style={{height: "450", display: "flex", justifyContent:"space-around"}}>
-        <Card className="card" style={{ width: "30%"}}>
+      <Col
+        span={22}
+        offset={1}
+        style={{
+          height: '450',
+          display: 'flex',
+          justifyContent: 'space-around',
+        }}
+      >
+        <Card className='card' style={{ width: '30%' }}>
+          Balance: {balance}
         </Card>
-        <Card className="card" style={{ width: "30%", height: "100%"}}>
-            <div style={{height: "100%",display: "flex", flexDirection:"column", justifyContent:"space-between"}}>
-              <div>
-                <img src={solanaLogo} alt="solana logo" style={{maxWidth:"30%"}}/>
-                <h2 className="subtext" style={{marginTop:"5px"}}>SOLANA</h2>
-              </div>
-              <div style={{marginTop: "5px", marginBottom:"5px"}}>
-                <h2 className="title" style={{marginBottom: 0}}>15.5%</h2>
-                <h3 style={{marginBottom: 0}}>Vault Rewards</h3>
-                <h5>(projected APY)</h5>
-              </div>
-              <Link to="/discover">
-                <Button className="tenderButton tenderButtonShade">Discover</Button>
-              </Link>
+        <Card className='card' style={{ width: '30%', height: '100%' }}>
+          <div
+            style={{
+              height: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <div>
+              <img
+                src={solanaLogo}
+                alt='solana logo'
+                style={{ maxWidth: '30%' }}
+              />
+              <h2 className='subtext' style={{ marginTop: '5px' }}>
+                SOLANA
+              </h2>
             </div>
+            <div style={{ marginTop: '5px', marginBottom: '5px' }}>
+              <h2 className='title' style={{ marginBottom: 0 }}>
+                15.5%
+              </h2>
+              <h3 style={{ marginBottom: 0 }}>Vault Rewards</h3>
+              <h5>(projected APY)</h5>
+            </div>
+            <ConnectButton
+              className='tenderButton tenderButtonShade'
+              onClick={() => handleClick('/discover')}
+            >
+              Stake
+            </ConnectButton>
+
+            {/* <Link to="/discover">
+                <Button className="tenderButton tenderButtonShade">Stake</Button>
+              </Link> */}
+          </div>
         </Card>
-        <Card className="card" style={{ width: "30%"}}>
-        </Card>
+        <Card className='card' style={{ width: '30%' }}></Card>
       </Col>
-        <Col span={10} offset={2}>
-          <div style={{marginTop: "100px", marginBottom: "70px"}}>
-          <img style={{maxWidth: "100%"}} src={potionHero} alt="potionHero"/>
-          </div>
-        </Col>
-        <Col span={10}>
-          <p className="subtext"> Tenderize is a liquid staking protocol. </p>
-          <p className="subtext"> It allows you to enjoy your farm-fresh, tender staking returns without the wait of an unbonding period (1-3 months in the case of Keep).</p>
-        </Col>
-        <Col span={24}>
-          <div style={{height: "70vh", marginBottom: "30px"}}>
-            <div className="table card">
-              <div>
-                <span className="subtext table-left">Step 1</span>
-                <p className="subtext">Order off of our select, stake menu.  Deposit your stake and let the tenderizing begin.</p>
-              </div>
-              <div>
-                <span className="subtext table-left">Step 2</span>
-                <p className="regulartext">Receive a freshly minted, tender token that represents your initial stake and any earned rewards.</p>
-              </div>
-              <div>
-                <span className="subtext table-left">Step 3</span>
-                <p className="regulartext">With your new tender token, you can skip the wait of your stake’s unbonding period.  Go utilize, liquidize, and collateralize...all while you tenderize.</p>
-              </div>
+      <Col span={10} offset={2}>
+        <div style={{ marginTop: '100px', marginBottom: '70px' }}>
+          <img style={{ maxWidth: '100%' }} src={potionHero} alt='potionHero' />
+        </div>
+      </Col>
+      <Col span={10}>
+        <p className='subtext'> Tenderize is a liquid staking protocol. </p>
+        <p className='subtext'>
+          {' '}
+          It allows you to enjoy your farm-fresh, tender staking returns without
+          the wait of an unbonding period (1-3 months in the case of Keep).
+        </p>
+      </Col>
+      <Col span={24}>
+        <div style={{ height: '70vh', marginBottom: '30px' }}>
+          <div className='table card'>
+            <div>
+              <span className='subtext table-left'>Step 1</span>
+              <p className='subtext'>
+                Order off of our select, stake menu. Deposit your stake and let
+                the tenderizing begin.
+              </p>
+            </div>
+            <div>
+              <span className='subtext table-left'>Step 2</span>
+              <p className='regulartext'>
+                Receive a freshly minted, tender token that represents your
+                initial stake and any earned rewards.
+              </p>
+            </div>
+            <div>
+              <span className='subtext table-left'>Step 3</span>
+              <p className='regulartext'>
+                With your new tender token, you can skip the wait of your
+                stake’s unbonding period. Go utilize, liquidize, and
+                collateralize...all while you tenderize.
+              </p>
             </div>
           </div>
-        </Col>
-        <Col span={12} offset={2}>
-        <div style={{marginTop: "80px", marginBottom: "100px"}}>
-          <img style={{maxWidth:"100%"}} src={friends} alt="friends"/>
-          </div>
-        </Col>
-        <Col span={8}>
-          <p className="subtext">Friends don’t let friends stake, unless they tenderize.</p>
-        </Col>
-        <Col span={24}>
-        <div className="builton" />
+        </div>
+      </Col>
+      <Col span={12} offset={2}>
+        <div style={{ marginTop: '80px', marginBottom: '100px' }}>
+          <img style={{ maxWidth: '100%' }} src={friends} alt='friends' />
+        </div>
+      </Col>
+      <Col span={8}>
+        <p className='subtext'>
+          Friends don’t let friends stake, unless they tenderize.
+        </p>
+      </Col>
+      <Col span={24}>
+        <div className='builton' />
       </Col>
     </Row>
   );
